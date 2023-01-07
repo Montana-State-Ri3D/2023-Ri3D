0e7cd1c6fcb8f4d208bf0019b44733ab5ebdeeef
Joshua Elmore <boyscout574@outlook.com>
2023-01-06 20:49:24 -0800

    Set Can ID

--- 2023_Ri3D_Robot_code/src/main/java/frc/robot/Constants.java
+++ 2023_Ri3D_Robot_code/src/main/java/frc/robot/Constants.java
@@ -9,8 +9,8 @@ public final class Constants {
     public static final int PDP_CAN_ID = 0;
 
     // Drive Motors
-    public static final int LEFT_FRONT_MOTOR = 4;
-    public static final int LEFT_BACK_MOTOR = 5;
-    public static final int RIGHT_FRONT_MOTOR = 6;
-    public static final int RIGHT_BACK_MOTOR = 7;
+    public static final int LEFT_FRONT_MOTOR = 1;
+    public static final int LEFT_BACK_MOTOR = 2;
+    public static final int RIGHT_FRONT_MOTOR = 3;
+    public static final int RIGHT_BACK_MOTOR = 4;
 }
